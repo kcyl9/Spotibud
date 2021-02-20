4211536100cc81412ffd343aafe91c6049318ea2
richmondvan <evan.ty.zheng@gmail.com>
2021-02-19 22:58:26 -0500

    Tried to query spotify, moved firebase database

--- src/index.js
+++ src/index.js
@@ -3,23 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import firebase from 'firebase';
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCdHlj6bH24v9JyLJmb2uHT4SfgDbk6utY",
-  authDomain: "spotibud.firebaseapp.com",
-  databaseURL: "https://spotibud-default-rtdb.firebaseio.com",
-  projectId: "spotibud",
-  storageBucket: "spotibud.appspot.com",
-  messagingSenderId: "171308756762",
-  appId: "1:171308756762:web:4a538418717f456ebe46a3",
-  measurementId: "G-41MB6MS2WG"
-}
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-var database = firebase.database();
 
 ReactDOM.render(
   <React.StrictMode>
