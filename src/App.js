326b15c33226f0db8d9ded4eca99af665851e8c8
ethan-kwon <ethan200304@gmail.com>
2021-02-19 22:26:20 -0500

    initialized firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import 'firebase/database';
+
+var firebase = require("firebase/app");
+require("firebase/database");
+
+firebase.intializeApp({ 
+  apiKey: "AIzaSyCdHlj6bH24v9JyLJmb2uHT4SfgDbk6utY",
+  authDomain: "spotibud.firebaseapp.com",
+  databaseURL: "https://spotibud-default-rtdb.firebaseio.com",
+  projectId: "spotibud",
+  storageBucket: "spotibud.appspot.com",
+  messagingSenderId: "171308756762",
+  appId: "1:171308756762:web:4a538418717f456ebe46a3",
+  measurementId: "G-41MB6MS2WG"
+})
+
+var database = firebase.database();
 
 function App() {
   return (
