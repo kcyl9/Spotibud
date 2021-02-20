6d97201e0be64ae315d5ca6d7a7d5dc11f7ed199
ethan-kwon <ethan200304@gmail.com>
2021-02-20 16:44:32 -0500

    building scroll features

--- src/Components/Chatbox.js
+++ src/Components/Chatbox.js
@@ -8,7 +8,8 @@ let dbRefMessagesList = dbRefObject;
 class Chatbox extends Component {
 
     state = {
-        messages: []
+        messages: [],
+        loadedMessages: []
     }
 
     constructor(props) {
@@ -19,10 +20,23 @@ class Chatbox extends Component {
 
     componentDidMount = () => {
         this.buildMessages();
+        this.loadMessages();
+    }
+
+    loadMessages() {
+        for (let index = 0; index < this.state.messages.length/2; index++) {
+            try {
+                this.state.loadedMessages.push(this.state.messages.pop())
+                console.log(this.state.loadedMessages)
+                console.log(this.state.messages)
+            } catch (error) {
+                console.log("no more to load")
+            }
+        }
     }
 
     buildMessages() {
-       dbRefMessagesList.on('child_added', snap => {
+        dbRefMessagesList.on('child_added', snap => {
            if (snap.val().message) {
                let messages = [...this.state.messages]
                messages.push({message: snap.val().message,
