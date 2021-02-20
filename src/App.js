a542f6087f792df722169447f7cebc5f04fc4d41
kcyl9 <kristyphone119@gmail.com>
2021-02-20 09:53:44 -0500

    Created main menu - JS

--- src/App.js
+++ src/App.js
@@ -19,11 +19,16 @@ function App() {
   getSpotifyTrackID();
   
   const listMessages = messagesDummy.map((text) => <li>{text}</li>);
+  //document.body.style.backgroundImage = "url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js')"
 
   return (
     <div className="App">
-      <ul id="listMessages">{listMessages}</ul>
-      <Textbox/>
+      <h1 class="App-text1">Drop Song URL Here</h1>
+      <td class="App-url-input"><input type="url"></input></td>
+      <img src="buds.svg" width = "20%" alt="best friends owo" class="App-logo"></img>
+      <p class="App-text2"><b>to meet spotibuddies</b></p>
+      {/* <ul id="listMessages">{listMessages}</ul>
+      <Textbox/> */}
     </div>
   );
 }
